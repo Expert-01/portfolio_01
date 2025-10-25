0ae19aaf1a3f025b40914417b065bdbacefdd8eb
Expert-01 <soalagideon@gmail.com>
2025-10-25 05:24:24 +0100

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -52,8 +52,8 @@ export default function About() {
               style={{ y: yTransform }}
               className="mb-12 sticky top-32"
             >
-              <h3 className="text-2xl md:text-3xl font-semibold text-cyan-400 mb-2">{skill.name}</h3>
-              <p className="text-lg md:text-xl leading-relaxed font-sans">
+              <h3 className="text-2xl md:text-3xl font-orbitron font-semibold text-cyan-400 mb-2">{skill.name}</h3>
+              <p className="text-lg md:text-xl leading-relaxed font-orbitron">
                 {skill.description}
               </p>
             </motion.div>
