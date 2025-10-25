ad6b0de7aa9071e4ec44a8fcbbf94a9096fd0b9b
Expert-01 <soalagideon@gmail.com>
2025-10-25 20:06:59 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ export default function Hero() {
       }}
     >
       {/* RippleGrid background */}
-      <div className=" inset-0 -z-10">
+      <div className="absolute inset-0 -z-10">
         <RippleGrid
           enableRainbow={false}
           gridColor="red"
